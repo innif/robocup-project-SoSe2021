367f6238ee6e2ef72e62133f4f1e3cd95fc4b20e
maksim <maksim.holly@gmail.com>
2021-07-09 15:53:20 +0200

    testing simspark vision

--- simspark_controller.py
+++ simspark_controller.py
@@ -1,11 +1,26 @@
 # Maksim
+# add PYTHONPATH
+import os
+import sys
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lecture_code'))
 
-class SimsparkController:
-    def __init__(self):
-        pass
+from spark_agent import SparkAgent
+
+
+class SimsparkController(SparkAgent):
+    #def __init__(self):
+        #pass
+
+    def think(self, perception):
+        print(perception.see)
+        return super(SimsparkController, self).think(perception)
 
     def foo(self):
         print("hi")
 
 
 simspark_controller = SimsparkController()
+
+if '__main__' == __name__:
+    agent = simspark_controller
+    agent.run()
