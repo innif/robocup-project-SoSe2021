9d00a152ba9f6288db378e57aca986effc87b302
Sebastian Eberlein <sebastian.eberlein@campus.tu-berlin.de>
2021-08-23 17:28:53 +0200

    Make walking_agent use naoqi rpc client

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import logging
 from threading import Thread
-from time import sleep
 
-from robolib import ServerAgent
 from walking_agent import WalkingAgent
 
 
@@ -11,19 +9,16 @@ def main():
 
     logger = logging.getLogger(__name__)
 
-    server_ip_address, server_port = 'localhost', 8888
-    server_uri = f"http://{server_ip_address}:{server_port}"
-
-    server = ServerAgent(ip_address=server_ip_address, port=server_port)
-    server_thread = Thread(target=server.run)
-    server_thread.start()
-    logger.info('Server started')
+    # Set server uri of naoqi rpc server
+    server_ip_address, server_port = '172.17.0.3', 8000
+    server_uri = "http://{}:{}".format(server_ip_address, server_port)
 
     client = WalkingAgent(server_uri=server_uri)
     logger.info('Client started')
-    sleep(2)
 
-    client.dance()
+    logger.info('Move to (2, 0)')
+    client.walk_to(2.0, 0, wait=True)
+    logger.info('Finished')
 
 
 if __name__ == '__main__':
--- robolib/__init__.py
+++ robolib/__init__.py
@@ -1,2 +1,3 @@
 from robolib.agents.agent_server import ServerAgent
 from robolib.agents.agent_client import ClientAgent
+from robolib.agents.naoqi_agent_client import NaoqiClientAgent
