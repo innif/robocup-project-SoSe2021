80462333271e766ac99fa8c26a83754bf40eefe9
Sebastian Eberlein <sebastian.eberlein@campus.tu-berlin.de>
2021-08-24 17:02:04 +0200

    Make cv_agent use naoqiclient

--- cv_agent.py
+++ cv_agent.py
@@ -1,15 +1,17 @@
 # Finn
-from simspark_controller import simspark_controller
+from robolib import NaoqiClientAgent
 
 
 class CVAgent:
-    def __init__(self):
-        simspark_controller.foo()
+    def __init__(self, server_uri):
+        self._agent = NaoqiClientAgent(server_uri)
         self.goal_center = (100, 100)  # Center of Goal in Pixels
         self.goal_size = (100, 200)  # width, height
+
+        self._agent.subscribe_to_cam(2, 11)
 
     def update(self, perception) -> None:
         pass
 
-
-cv_agent = CVAgent()
+    def get_image(self):
+        return self._agent.get_image()
