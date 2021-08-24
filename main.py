b40848188442ebc8967149cb1df7392c0c5b9a92
Sebastian Eberlein <sebastian.eberlein@campus.tu-berlin.de>
2021-08-24 17:05:08 +0200

    Add basic image retrieval to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
+import math
 from threading import Thread
+from PIL import Image
 
+from cv_agent import CVAgent
 from walking_agent import WalkingAgent
 
 
@@ -10,14 +13,29 @@ def main():
     logger = logging.getLogger(__name__)
 
     # Set server uri of naoqi rpc server
-    server_ip_address, server_port = '172.17.0.3', 8000
+    server_ip_address, server_port = '172.17.0.2', 8000
     server_uri = "http://{}:{}".format(server_ip_address, server_port)
 
-    client = WalkingAgent(server_uri=server_uri)
-    logger.info('Client started')
+    walking_agent = WalkingAgent(server_uri=server_uri)
+    cv_agent = CVAgent(server_uri=server_uri)
 
-    logger.info('Move to (2, 0)')
-    client.walk_to(2.0, 0, wait=True)
+    logger.info('walking_agent started')
+
+    nao_image = cv_agent.get_image()
+    image_width = nao_image[0]
+    image_height = nao_image[1]
+    array = nao_image[6]
+    image_string = bytes(array, encoding='ascii')
+
+    # Create a PIL Image from our pixel array.
+    im = Image.frombytes("RGB", (image_width, image_height), image_string)
+
+    # Save the image.
+    im.save("camImage.png", "PNG")
+
+    im.show()
+
+    walking_agent.walk_to(1, -0.50, 0, wait=True)
     logger.info('Finished')
 
 
