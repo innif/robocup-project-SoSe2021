850f39aacc3cabad98bd28ad20566d68d6310531
maksim <maksim.holly@gmail.com>
2021-08-26 00:19:42 +0200

    implement navigation agent and rework main

--- main.py
+++ main.py
@@ -2,15 +2,31 @@ import logging
 
 from cv_agent import CVAgent
 from walking_agent import WalkingAgent
+from navigation_agent import NavigationAgent
 
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
 
     logger = logging.getLogger(__name__)
+    
+    server_ip_address, server_port = '127.0.0.1', 8000
+    server_uri = "http://{}:{}".format(server_ip_address, server_port)
+    
+    navigation_agent = NavigationAgent(server_uri=server_uri)
+
+    logging.info('Start looped procedure to turn robot and walk to goal.')
+    
+    while True:
+        navigation_agent.run()
+
+def main_o():
+    logging.getLogger().setLevel(logging.INFO)
+
+    logger = logging.getLogger(__name__)
 
     # Set server uri of naoqi rpc server
-    server_ip_address, server_port = '172.17.0.2', 8000
+    server_ip_address, server_port = '127.0.0.1', 8000
     server_uri = "http://{}:{}".format(server_ip_address, server_port)
 
     walking_agent = WalkingAgent(server_uri=server_uri)
@@ -22,6 +38,7 @@ def main():
     logging.info('walking started')
     walking_agent.walk_to(1, -0.50, 0)
     logging.info('Finished')
+
 
 
 if __name__ == '__main__':
--- navigation_agent.py
+++ navigation_agent.py
@@ -1,25 +1,65 @@
 # Maksim
-from cv_agent import cv_agent
-from walking_agent import walking_agent
+from cv_agent import CVAgent
+from walking_agent import WalkingAgent
 
+TARGET_COLOR = "yellow"
 
 class NavigationAgent:
-    def __init__(self):
-        pass
+    def __init__(self, server_uri):
+        self.__cv_agent = CVAgent(server_uri=server_uri)
+        self.__walking_agent = WalkingAgent(server_uri=server_uri)
+    
+    def run(self):
+        turn_to_goal()
+        walk_to_goal()
+     
+    def update(self, target):
+        self.__cv_agent.update(target)
 
-    def update(self, perception):
-        cv_agent.update(perception)
-        x, y, z = self.__calc_walking_dir()
-        walking_agent.walk_to(x, y, z)
+    def turn_to_goal(self):
+        """
+        Turns the robot towards the goal until center of the goal is approximately in center of the robot's camera image.
+        Please note: The robot's movement axis are not equal to the image's axis. Instead of just the head, the whole
+        robot has to turn so the movement direction is correct afterwars.
+        Image axis are like this:
+         
+         ^ y-Axis
+         |
+         |
+         |
+         |
+         |-----------> x-Axis
+        In order to "move" the goal on the x-axis of the camera image, the robot has to turn around its z-axis using the theta angle.
+        The robot axis definitions can be found at http://doc.aldebaran.com/1-14/naoqi/motion/index.html#axis-definition
+        """
+        self.update(TARGET_COLOR)
+        goal_center = self.__cv_agent.goal_center
+        while goal_center[0] == None:
+            self.__walking_agent.walk_to(0, 0, theta=0.1, wait=True)
+            goal_center = self.__cv_agent.goal_center
+        while(not -10 <= goal_center[0] <= 10):
+            if(goal_center < 0):
+                self.__walking_agent.walk_to(0, 0, theta=0.1, wait=True)
+            else:
+                self.__walking_agent.walk_to(0, 0, theta=-0.1, wait=True)
+            self.update(TARGET_COLOR)
+            goal_center = self.__cv_agent.goal_center
+            
+    def walk_to_goal(self):
+        """
+        Robot walks towards the goal as long as the goal size is below a certain threshold.
+        Please note: The robot's x-movement axis is not equal to the image's x-axis
+        """
+        goal_size_threshold = (100, 50)
+        self.update(TARGET_COLOR)
+        goal_size = self.__cv_agent.goal_size
+        if(goal_size == None):
+            return
+        while goal_size < goal_size_threshold:
+            self.__walking_agent.walk_to(1, 0, theta=0, wait=True)
+            self.update(TARGET_COLOR)
+            goal_size = self.__cv_agent.goal_size
 
-    def __calc_walking_dir(self) -> tuple:
-        goal_size = cv_agent.goal_size
-        goal_center = cv_agent.goal_center
-        # TODO calculation
-        x = 1
-        y = 2
-        z = 3
-        return x, y, z
 
 
 navigation_agent = NavigationAgent()
